8e7726616ec2172926a5cb8ba09f2a6ed34498ac
GeiseMaurício <geise.mauricio@gmail.com>
2022-11-11 12:54:52 -0400

    Create e2e tests with cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,14 @@
+
+
+it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+  
+    cy.login(
+      Cypress.env('USER_EMAIL'),
+      Cypress.env('USER_PASSWORD'),
+      { cacheSession: false }
+    )
+  
+    cy.wait(500)
+    cy.wait('@getNotes')
+  })
---
+++ cypress/support/index.js
@@ -0,0 +1,4 @@
+import "./commands";
+require("cypress-mailosaur");
+import 'cypress-file-upload';
+import 'cypress-iframe'
